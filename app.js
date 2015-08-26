622793e7bd1bbf3da872b6418009b937d0d39d6e
Allan Nielsen <allannielsen@Allans-MacBook.local>
2015-08-25 20:09:20 -0500

    bootstrap added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
+var boots = require('bootstrap');
+
 app.use(express.static(__dirname + '/resume'));
+app.use(express.static(__dirname + '/bootstrap/js/*.js'));
 
 app.get('/', function (req, res){
     res.writeHead(200);
