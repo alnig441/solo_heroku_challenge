89199468a1930892d1557d19d0a0eda5daae507b
Allan Nielsen <allannielsen@Allans-MacBook.local>
2015-08-25 20:19:38 -0500

    bootstrap correction 4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 //var boots = require('bootstrap');
 
 app.use(express.static(__dirname + '/resume'));
-//app.use(express.static(__dirname + '/node_modules/bootstrap/js/*.js'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/js/*.js'));
 
 app.get('/', function (req, res){
     res.writeHead(200);
